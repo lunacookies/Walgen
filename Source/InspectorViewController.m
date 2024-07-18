fa637658c11df591a5c1fcc8fe2b02da290b08ed
Luna Razzaghipour <luna@xoria.org>
2024-07-18 17:49:30 +1000

    Add color well to inspector

--- Source/InspectorViewController.m
+++ Source/InspectorViewController.m
@@ -1,21 +1,39 @@
 @implementation InspectorViewController
 {
-	NSTextField *label;
+	NSGridView *gridView;
 }
 
 - (void)viewDidLoad
 {
 	self.title = @"Inspector";
 
-	label = [NSTextField labelWithString:@"hello world"];
-	label.translatesAutoresizingMaskIntoConstraints = NO;
-	[self.view addSubview:label];
-	[NSLayoutConstraint activateConstraints:@[
-		[label.topAnchor constraintEqualToAnchor:self.view.topAnchor],
-		[label.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor],
-		[label.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor],
-		[label.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor],
+	NSTextField *label = [NSTextField labelWithString:@"Background Color:"];
+	NSColorWell *backgroundColorWell = [NSColorWell colorWellWithStyle:NSColorWellStyleDefault];
+	backgroundColorWell.target = self;
+	backgroundColorWell.action = @selector(backgroundColorDidChange:);
+
+	gridView = [NSGridView gridViewWithViews:@[
+		@[ label, backgroundColorWell ],
 	]];
+	gridView.yPlacement = NSGridCellPlacementCenter;
+
+	gridView.translatesAutoresizingMaskIntoConstraints = NO;
+	CGFloat padding = 10;
+	[self.view addSubview:gridView];
+	[NSLayoutConstraint activateConstraints:@[
+		[gridView.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:padding],
+		[gridView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor
+		                                       constant:padding],
+		[gridView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor
+		                                        constant:-padding],
+		[gridView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor
+		                                      constant:-padding],
+	]];
+}
+
+- (void)backgroundColorDidChange:(NSColorWell *)backgroundColorWell
+{
+	NSLog(@"%@", backgroundColorWell.color);
 }
 
 @end
