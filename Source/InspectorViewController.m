d574dcd1227e9bf546a3dad29303234e2c495e6f
Luna Razzaghipour <luna@xoria.org>
2024-07-19 12:51:51 +1000

    Replace manual padding with `layoutMarginsGuide`

--- Source/InspectorViewController.m
+++ Source/InspectorViewController.m
@@ -69,16 +69,13 @@
 	firstColumn.xPlacement = NSGridCellPlacementTrailing;
 
 	gridView.translatesAutoresizingMaskIntoConstraints = NO;
-	CGFloat padding = 10;
 	[self.view addSubview:gridView];
+	NSLayoutGuide *guide = self.view.layoutMarginsGuide;
 	[NSLayoutConstraint activateConstraints:@[
-		[gridView.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:padding],
-		[gridView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor
-		                                       constant:padding],
-		[gridView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor
-		                                        constant:-padding],
-		[gridView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor
-		                                      constant:-padding],
+		[gridView.topAnchor constraintEqualToAnchor:guide.topAnchor],
+		[gridView.leadingAnchor constraintEqualToAnchor:guide.leadingAnchor],
+		[gridView.trailingAnchor constraintEqualToAnchor:guide.trailingAnchor],
+		[gridView.bottomAnchor constraintEqualToAnchor:guide.bottomAnchor],
 
 		[noiseInfluenceSlider.widthAnchor constraintGreaterThanOrEqualToConstant:100],
 	]];
